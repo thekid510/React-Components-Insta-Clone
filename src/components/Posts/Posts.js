03252a1eb41cf84330495058b8e33340fa8b0987
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-03-16 17:13:05 -0700

    added comment section

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -8,8 +8,7 @@ const Posts = ({ likePost, posts }) => {
 
   return(
     <div className='posts-container-wrapper'>
-      {/* Map through the posts array returning a Post component at each iteration */}
-      {/* Check the implementation of Post to see what props it requires! */}
+
     
       {
         posts.map((post) => (
